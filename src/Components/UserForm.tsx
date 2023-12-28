fd23e7a5538f9946e4fa612533725c92b6ba342e
SohamTcog <soham.kulkarni@tcognition.com>
2023-12-28 10:40:50 +0530

    get data form

--- src/Components/UserForm.tsx
+++ src/Components/UserForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+import { Link } from 'react-router-dom';
+import "./UserForm.css"
 const UserForm: React.FC = () => {
   const [formData, setFormData] = useState({
     name: '',
@@ -19,49 +20,54 @@ const UserForm: React.FC = () => {
     try {
       await axios.post('http://localhost:8090/StudentDetails-0.0.1-SNAPSHOT/saveDetails', formData);
       setFormData({
-         name: '',
-    city: '',
-    company: '',
+        name: '',
+        city: '',
+        company: '',
       });
-
+      alert('Data Saved');
     } catch (error) {
       alert('Error saving user data:');
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>First Name:</label>
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-        />
+    <div className="form-container">
+      <form onSubmit={handleSubmit} className="user-form">
+        <div className="form-group">
+          <label>Name: </label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>City: </label>
+          <input
+            type="text"
+            name="city"
+            value={formData.city}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Company: </label>
+          <input
+            type="text"
+            name="company"
+            value={formData.company}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <div className="link-container">
+        <Link to="/getData">List of Employees</Link>
       </div>
-      <div>
-        <label>Address:</label>
-        <input
-          type="text"
-          name="city"
-          value={formData.city}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label>Company Name:</label>
-        <input
-          type="text"
-          name="company"
-          value={formData.company}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+    </div>
   );
 };
 
