4d2d906f2b8fff11520f06f177fcc74d59ced212
Roshan-Farkate <138759751+Roshan-Farkate@users.noreply.github.com>
2024-01-19 14:54:50 +0530

    Update IncDec.tsx1

--- src/Components/IncDec.tsx
+++ src/Components/IncDec.tsx
@@ -22,8 +22,7 @@ const IncDec = () => {
 
   return (
         <div className="App">
-      <h1>{count}</h1>
-        <button>Increment++++</button>    
+      <h1>{count}</h1> 
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
